ff30ca85e300ab2089e53ca5e32f20d40c69e0f6
haoran <haoran.mc@outlook.com>
2023-01-03 00:05:33 +0800

    construction

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -4,7 +4,7 @@ alias ra='ranger'
 alias vsc='code'
 alias n='nvim'
 alias e='emacs -nw'
-alias v='vim'
+alias v='vim -u ~/dotfiles/vim/tiny.vim'
 alias vi='vim'
 alias vim='vim'
 alias V='vim -u NONE -N'
@@ -14,3 +14,5 @@ alias t='tree'
 alias nnn='source ~/haoran/Code/Editor/Git/push.sh'
 alias NEXTLINE='echo ""'
 alias gm="go run main.go"
+
+alias win="cd /mnt/c/Users/haora"
