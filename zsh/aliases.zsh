5f69a7bbf038eef102bd83fd8ed96cc3477a57b8
haoran <haoran.mc@outlook.com>
2023-01-03 00:38:35 +0800

    construction

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -15,4 +15,9 @@ alias nnn='source ~/haoran/Code/Editor/Git/push.sh'
 alias NEXTLINE='echo ""'
 alias gm="go run main.go"
 
+alias hard_pull="git fetch --all && git reset --hard origin/main && git pull"
+alias hard_push="git commit --amend && git push -f"
+
 alias win="cd /mnt/c/Users/haora"
+# alias upv="cp ~/dotfiles/vim/windows-gui-for-algorithm.vim /mnt/c/Users/haora/.vimrc"
+alias upv="rm -rf /mnt/c/Users/haora/dotfiles && cp -r ~/dotfiles /mnt/c/Users/haora/dotfiles"
