e3620bbdbbd26681bad07f31f8482725c5a2a7d0
haoran <haoran.mc@outlook.com>
2023-03-24 03:20:06 +0800

    vim :term

--- bash/env.sh
+++ bash/env.sh
@@ -1,3 +1,11 @@
 # PATH=$PATH:/opt/MySpecialApplication/bin; export PATH
 
+# golang
+export GOROOT=/usr/local/go
+export GOPATH=$HOME/go
+export GOBIN=$GOPATH/bin
+export PATH=$PATH:$GOBIN:$GOROOT/bin
+export GO111MODULE=on
+export GOPROXY=https://goproxy.io,direct
+
 export EDITOR="vim"
