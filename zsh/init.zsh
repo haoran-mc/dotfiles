d6441f06bbb179836e8154694bc39f0b34d778ec
haoran <haoran.mc@outlook.com>
2023-08-13 21:28:08 +0800

    prefect zsh

--- zsh/init.zsh
+++ zsh/init.zsh
@@ -6,12 +6,10 @@ if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]
 fi
 
 source ~/dotfiles/zsh/init/ui.zsh
-source ~/dotfiles/zsh/init/default.zsh
-source ~/dotfiles/zsh/init/aliases.zsh
+source ~/dotfiles/zsh/init/option.zsh
 source ~/dotfiles/zsh/init/env.zsh
 source ~/dotfiles/zsh/init/funcs.zsh
-source ~/dotfiles/zsh/init/plugins.zsh
-source ~/dotfiles/zsh/init/os.zsh
+source ~/dotfiles/zsh/init/plugin.zsh
 
 # Generated for envman. Do not edit.
 [ -s "$HOME/.config/envman/load.sh" ] && source "$HOME/.config/envman/load.sh"
@@ -19,9 +17,3 @@ source ~/dotfiles/zsh/init/os.zsh
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
 
-
-
-
-
-
-source /Users/haoran/.config/broot/launcher/bash/br
