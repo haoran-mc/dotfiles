ec913e11a333de4244d0fd6569b6c16438b56329
刘浩然 <haoran@ran-macbook-air.local>
2023-11-22 13:16:29 +0800

    add scripts for setup my new env

--- osx-setup.sh
+++ osx-setup.sh
@@ -1,34 +1,24 @@
 #!/bin/bash
 
+# https://github.com/nithinbekal/setup-scripts
+
 # install homebrew - this will also install xcode command line tools
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-brew install ack ctags git imagemagick mongodb htop zsh vim macvim elixir
-brew install tmux reattach-to-user-namespace
+brew install ctags git htop zsh vim macvim elixir
+brew install -q tmux reattach-to-user-namespace
 
 # use cask for installing GUI apps
-brew install caskroom/cask/brew-cask
-brew cask install dropbox iterm2 seil sublime-text
+brew cask install iterm2 sublime-text --cask
 
 # git
 git config --global user.name "haoran"
 git config --global user.email "haoran.mc@outlook.com"
 
-# mongodb
-mkdir -p ~/Library/LaunchAgents
-ln -sfv /usr/local/opt/mongodb/*.plist ~/Library/LaunchAgents
-launchctl load ~/Library/LaunchAgents/homebrew.mxcl.mongodb.plist
-
 # configure zsh
 touch ~/.env.local
 chsh -s /bin/zsh
 curl -L http://install.ohmyz.sh | sh
-
-# configure vim
-mkdir -p ~/.vim/bundle
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim +PluginInstall +qall
-mkdir -p ~/.tmp
 
 # install RVM
 \curl -sSL https://get.rvm.io | bash -s stable
@@ -45,4 +35,4 @@ ssh-keygen -t rsa -C "haoran.mc@outlook.com"
 # set up postgresql
 brew install postgresql
 brew services start postgresql
-initdb /usr/local/var/postgres
+initdb --locale=C -E UTF-8 /opt/homebrew/var/postgresql
---
+++ scripts/go-setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+VERSION="1.21.1"
+
+source .bashrc
+wget https://storage.googleapis.com/golang/go$VERSION.linux-amd64.tar.gz
+tar xvfz go$VERSION.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go
+sudo mv go /usr/local/
+
+go install golang.org/x/tools/cmd/goimports@latest
+go install github.com/nsf/gocode@latest
+go install golang.org/x/tools/gopls@latest
+
+rm go$VERSION.linux-amd64.tar.gz
