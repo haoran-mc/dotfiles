4c0270a7a313d0223ec88cfc06bddf1b1fac094f
haoran <haoran.mc@outlook.com>
2023-11-30 19:02:34 +0800

    fix changed color after lsp attach

--- nvim/lua/basic/autocmd.lua
+++ nvim/lua/basic/autocmd.lua
@@ -3,8 +3,8 @@ vim.api.nvim_create_autocmd({ "BufEnter" }, {
   pattern = { "*" },
   callback = function()
     vim.opt.formatoptions = vim.opt.formatoptions
-      - "o" -- O and o, don't continue comments
-      + "r" -- But do continue when pressing enter.
+    - "o" -- O and o, don't continue comments
+    + "r" -- But do continue when pressing enter.
   end,
 })
 
@@ -35,6 +35,14 @@ vim.api.nvim_create_autocmd("BufRead", {
       command = "normal! zx",
     })
   end,
+})
+
+-- https://github.com/neovim/nvim-lspconfig/issues/2552#issuecomment-1506704775
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    client.server_capabilities.semanticTokensProvider = nil
+  end
 })
 
 ---------------[[
