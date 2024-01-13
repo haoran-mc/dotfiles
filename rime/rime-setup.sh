8037b6c214c33299057982df337b110f0b040367
haoran <haoran.mc@outlook.com>
2024-01-14 02:23:59 +0800

    rime squirrel style

--- rime/rime-setup.sh
+++ rime/rime-setup.sh
@@ -3,4 +3,5 @@
 if [[ `uname` == 'Darwin' ]]; then
     ln -s ~/dotfiles/rime/default.custom.yaml ~/Library/Rime/default.custom.yaml
     ln -s ~/dotfiles/rime/double_pinyin_flypy.custom.yaml ~/Library/Rime/double_pinyin_flypy.custom.yaml
+    ln -s ~/dotfiles/rime/squirrel.custom.yaml ~/Library/Rime/squirrel.custom.yaml
 fi
