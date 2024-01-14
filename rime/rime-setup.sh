391074feb7f640ae065f4a5a9d9b5f07e4851a61
haoran <haoran.mc@outlook.com>
2024-01-14 21:00:08 +0800

    ibus-rime in arch

--- rime/rime-setup.sh
+++ rime/rime-setup.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+# arch 下配置 fcitx5 好难啊，头都整晕了，先使用 ibus 吧。
+
+
 if [[ `uname` == 'Darwin' ]]; then
+    ln -s ~/dotfiles/rime/squirrel.custom.yaml ~/Library/Rime/squirrel.custom.yaml
     ln -s ~/dotfiles/rime/default.custom.yaml ~/Library/Rime/default.custom.yaml
     ln -s ~/dotfiles/rime/double_pinyin_flypy.custom.yaml ~/Library/Rime/double_pinyin_flypy.custom.yaml
-    ln -s ~/dotfiles/rime/squirrel.custom.yaml ~/Library/Rime/squirrel.custom.yaml
     ln -s ~/dotfiles/rime/symbols_caps_s.yaml ~/Library/Rime/symbols_caps_s.yaml
+elif [[ `uname` == 'Linux' ]]; then
+    ln -s ~/dotfiles/rime/default.custom.yaml ~/.config/ibus/rime/default.custom.yaml
+    ln -s ~/dotfiles/rime/double_pinyin_flypy.custom.yaml ~/.config/ibus/rime/double_pinyin_flypy.custom.yaml
+    ln -s ~/dotfiles/rime/symbols_caps_s.yaml ~/.config/ibus/rime/symbols_caps_s.yaml
 fi
