a2f09619ef562036f59f1f37ff2c3eb482879af4
haoran <haoran.mc@outlook.com>
2024-01-16 19:00:25 +0800

    rime add opencc

--- rime/rime-setup.sh
+++ rime/rime-setup.sh
@@ -9,6 +9,7 @@ if [[ `uname` == 'Darwin' ]]; then
     ln -s ~/dotfiles/rime/double_pinyin_flypy.custom.yaml ~/Library/Rime/double_pinyin_flypy.custom.yaml
     ln -s ~/dotfiles/rime/symbols_caps_s.yaml ~/Library/Rime/symbols_caps_s.yaml
     ln -s ~/dotfiles/rime/custom_phrase.txt ~/Library/Rime/custom_phrase.txt
+    ln -s ~/dotfiles/rime/opencc ~/Library/Rime/opencc
 elif [[ `uname` == 'Linux' ]]; then
     ln -s ~/dotfiles/rime/default.custom.yaml ~/.config/ibus/rime/default.custom.yaml
     ln -s ~/dotfiles/rime/double_pinyin_flypy.custom.yaml ~/.config/ibus/rime/double_pinyin_flypy.custom.yaml
