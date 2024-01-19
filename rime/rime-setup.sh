5dc4a8752b39efce974da3fc7fd973603fccc482
haoran <haoran.mc@outlook.com>
2024-01-19 21:31:34 +0800

    rime add dicts
    磨合完毕再推送字典文件

--- rime/rime-setup.sh
+++ rime/rime-setup.sh
@@ -12,6 +12,8 @@ if [[ `uname` == 'Darwin' ]]; then
     __link_file ~/dotfiles/rime/symbols.yaml ~/Library/Rime/symbols.yaml
     __link_file ~/dotfiles/rime/custom_phrase.txt ~/Library/Rime/custom_phrase.txt
     __link_file ~/dotfiles/rime/opencc ~/Library/Rime/opencc
+    __link_file ~/dotfiles/rime/rime_ice.dict.yaml ~/Library/Rime/rime_ice.dict.yaml
+    __link_file ~/dotfiles/rime/cn_dicts ~/Library/Rime/cn_dicts
 elif [[ `uname` == 'Linux' ]]; then
     __link_file ~/dotfiles/rime/default.custom.yaml ~/.config/ibus/rime/default.custom.yaml
     __link_file ~/dotfiles/rime/double_pinyin_flypy.custom.yaml ~/.config/ibus/rime/double_pinyin_flypy.custom.yaml
