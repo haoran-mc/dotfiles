21eff4992214623f86217ef46f509c67447f32f3
haoran <haoran.mc@outlook.com>
2024-02-12 01:59:06 +0800

    rime add huma config

--- rime/rime-setup.sh
+++ rime/rime-setup.sh
@@ -14,6 +14,31 @@ if [[ `uname` == 'Darwin' ]]; then
     __link_file ~/dotfiles/rime/opencc ~/Library/Rime/opencc
     __link_file ~/dotfiles/rime/rime_ice.dict.yaml ~/Library/Rime/rime_ice.dict.yaml
     __link_file ~/dotfiles/rime/cn_dicts ~/Library/Rime/cn_dicts
+
+    __link_file ~/dotfiles/rime/lua ~/Library/Rime/lua
+    __link_file ~/dotfiles/rime/rime.lua ~/Library/Rime/rime.lua
+
+    __link_file ~/dotfiles/rime/core2022.dict.yaml ~/Library/Rime/core2022.dict.yaml
+    # __link_file ~/dotfiles/rime/core2022.schema.yaml ~/Library/Rime/core2022.schema.yaml
+    __link_file ~/dotfiles/rime/easy_english.dict.yaml ~/Library/Rime/easy_english.dict.yaml
+    # __link_file ~/dotfiles/rime/easy_english.schema.yaml ~/Library/Rime/easy_english.schema.yaml
+
+    __link_file ~/dotfiles/rime/PY_c.schema.yaml ~/Library/Rime/PY_c.schema.yaml
+    # __link_file ~/dotfiles/rime/PY_c.dict.yaml ~/Library/Rime/PY_c.dict.yaml
+    __link_file ~/dotfiles/rime/PY_c.txt ~/Library/Rime/PY_c.txt
+
+    __link_file ~/dotfiles/rime/tiger.custom.yaml ~/Library/Rime/tiger.custom.yaml
+    __link_file ~/dotfiles/rime/tiger.dict.yaml ~/Library/Rime/tiger.dict.yaml
+    __link_file ~/dotfiles/rime/tiger.extended.dict.yaml ~/Library/Rime/tiger.extended.dict.yaml
+    __link_file ~/dotfiles/rime/tiger.schema.yaml ~/Library/Rime/tiger.schema.yaml
+
+    # __link_file ~/dotfiles/rime/tigress.custom.yaml ~/Library/Rime/tigress.custom.yaml
+    # __link_file ~/dotfiles/rime/tigress.dict.yaml ~/Library/Rime/tigress.dict.yaml
+    # __link_file ~/dotfiles/rime/tigress.extended.dict.yaml ~/Library/Rime/tigress.extended.dict.yaml
+    # __link_file ~/dotfiles/rime/tigress.schema.yaml ~/Library/Rime/tigress.schema.yaml
+    # __link_file ~/dotfiles/rime/tigress_ci.dict.yaml ~/Library/Rime/tigress_ci.dict.yaml
+    # __link_file ~/dotfiles/rime/tigress_simp_ci.dict.yaml ~/Library/Rime/tigress_simp_ci.dict.yaml
+
 elif [[ `uname` == 'Linux' ]]; then
     __link_file ~/dotfiles/rime/default.custom.yaml ~/.config/ibus/rime/default.custom.yaml
     __link_file ~/dotfiles/rime/double_pinyin_flypy.custom.yaml ~/.config/ibus/rime/double_pinyin_flypy.custom.yaml
