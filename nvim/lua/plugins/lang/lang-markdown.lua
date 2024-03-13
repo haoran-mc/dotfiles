80dc6a8ab0ded071bec0ee2a36492f3b36bf7806
haoran <haoran.mc@outlook.com>
2024-03-13 22:19:56 +0800

    nvim remove markdown-preview

--- nvim/lua/plugins/lang/lang-markdown.lua
+++ nvim/lua/plugins/lang/lang-markdown.lua
@@ -5,16 +5,6 @@ return {
     ft = "markdown",
   },
   {
-    "iamcco/markdown-preview.nvim",
-    build = "cd app && npm install",
-    keys = {
-      { "<leader>mp", "<cmd>MarkdownPreview<cr>", "n", { silent = true } },
-    },
-    init = function()
-      vim.g.mkdp_filetypes = { "markdown" }
-    end,
-  },
-  {
 
     "dhruvasagar/vim-table-mode",
     keys = {
