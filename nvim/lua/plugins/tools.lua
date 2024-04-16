7c08049ae29e558e09ae5ba021e2893dde2d3625
haoran <haoran.mc@outlook.com>
2024-04-16 15:06:55 +0800

    nvim use interestingwords.nvim

--- nvim/lua/plugins/tools.lua
+++ nvim/lua/plugins/tools.lua
@@ -6,10 +6,18 @@ return {
     },
   },
   {
-    "lfv89/vim-interestingwords", -- <leader>k
-    config = function ()
-      vim.g['interestingWordsGUIColors'] = {
-        '#A4E57E', '#8CCBEA', '#FFDB72', '#FF7272', '#FFB3FF', '#9999FF'
+    "Mr-LLLLL/interestingwords.nvim", -- <leader>k
+    config = function()
+      require("interestingwords").setup {
+        colors = { '#A4E57E', '#8CCBEA', '#FFDB72', '#ff0000', '#FFB3FF', '#aeee00' },
+        -- colors = { '#FF7272', '#b88823', '#ffa724', '#ff2c4b' },
+        search_count = true,
+        navigation = true,
+        scroll_center = true,
+        search_key = "<leader>m",
+        cancel_search_key = "<leader>M",
+        color_key = "<leader>k",
+        cancel_color_key = "<leader>K",
       }
     end
   }
