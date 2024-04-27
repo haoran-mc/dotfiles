f5013dd1b8c54902ae6a0e8be516fe33dd02740a
haoran <haoran.mc@outlook.com>
2024-04-27 18:37:23 +0800

    nvim use my forked repo interestingwords.nvim

--- nvim/lua/plugins/tools.lua
+++ nvim/lua/plugins/tools.lua
@@ -6,16 +6,11 @@ return {
     },
   },
   {
-    "Mr-LLLLL/interestingwords.nvim", -- <leader>k
+    "haoran-mc/interestingwords.nvim", -- <leader>k
     config = function()
       require("interestingwords").setup {
         colors = { '#A4E57E', '#8CCBEA', '#FFDB72', '#ff0000', '#FFB3FF', '#aeee00' },
         -- colors = { '#FF7272', '#b88823', '#ffa724', '#ff2c4b' },
-        search_count = true,
-        navigation = true,
-        scroll_center = true,
-        search_key = "<leader>m",
-        cancel_search_key = "<leader>M",
         color_key = "<leader>k",
         cancel_color_key = "<leader>K",
       }
