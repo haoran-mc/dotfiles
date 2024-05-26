3089161e45629ca0677037ae7764e5913d0ec797
haoran <haoran.mc@outlook.com>
2024-05-26 19:15:20 +0800

    nvim adding format plugin conform.nvim

--- nvim/lua/plugins/code.lua
+++ nvim/lua/plugins/code.lua
@@ -62,7 +62,41 @@ return {
 		keys = {
 			{ "<leader>cs", "<cmd>SymbolsOutline<cr>", "n", { silent = true } },
 		},
-		opts = {}
+		opts = {},
+	},
+	{
+		"stevearc/conform.nvim",
+		event = "VeryLazy",
+		ft = { "lua", "yaml", "python" },
+		config = function()
+			require("conform").setup({
+				format_after_save = {
+					lsp_fallback = true,
+				},
+				log_level = vim.log.levels.ERROR,
+				notify_on_error = true,
+				formatters_by_ft = {
+					lua = { "stylua" },
+					-- Conform will run multiple formatters sequentially
+					go = { "goimports", "gofumpt" },
+					-- Use a sub-list to run only the first available formatter
+					javascript = { { "prettierd", "prettier" } },
+					-- You can use a function here to determine the formatters dynamically
+					python = function(bufnr)
+						if require("conform").get_formatter_info("ruff_format", bufnr).available then
+							return { "isort", "ruff_format" }
+						else
+							return { "isort", "black" }
+						end
+					end,
+					json = { "jq" },
+					-- Use the "*" filetype to run formatters on all filetypes.
+					["*"] = { "codespell" },
+					-- Use the "_" filetype to run formatters on filetypes that don't
+					-- have other formatters configured.
+					["_"] = { "trim_whitespace" },
+				},
+			})
+		end,
 	},
 }
-
